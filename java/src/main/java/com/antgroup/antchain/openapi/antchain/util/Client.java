36dfa886667c79a5a0c9924548d72f83c5a595fa
yndu13 <yndu13@gmail.com>
2021-01-12 15:18:47 +0800

    fix util

--- java/src/main/java/com/antgroup/antchain/openapi/antchain/util/Client.java
+++ java/src/main/java/com/antgroup/antchain/openapi/antchain/util/Client.java
@@ -3,18 +3,32 @@ package com.antgroup.antchain.openapi.antchain.util;
 
 import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
-import javax.security.auth.login.AccountException;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.text.SimpleDateFormat;
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import com.alibaba.fastjson.*;
+import com.aliyun.tea.TeaModel;
 import org.apache.commons.io.IOUtils;
 
 public class Client {
+    private static final Pattern ENCODED_CHARACTERS_PATTERN;
+    private static final String DEFAULT_ENCODING = "UTF-8";
+
+
+    static {
+        StringBuilder pattern = new StringBuilder();
+
+        pattern.append(Pattern.quote("+")).append("|").append(Pattern.quote("*")).append("|")
+                .append(Pattern.quote("%7E")).append("|").append(Pattern.quote("%2F"));
+
+        ENCODED_CHARACTERS_PATTERN = Pattern.compile(pattern.toString());
+    }
 
     /**
      * Get timestamp
@@ -40,16 +54,18 @@ public class Client {
     public static Boolean hasError(String res, String secret) throws Exception {
         JSONObject response = JSONObject.parseObject(res);
         if (response == null && response.get("response") == null) {
-            return false;
+            return true;
         }
         Map<String, Object> map = (Map<String, Object>) response.get("response");
-        if (map != null && !"ok".equalsIgnoreCase(String.valueOf(map.get("result_code")))) {
+        if (map == null) {
             return true;
         }
-        String content = extractStringToSign(res);
-        String sign = sign(content, secret);
-        if (!response.get("sign").equals(sign)) {
-            return true;
+        if ("ok".equalsIgnoreCase(String.valueOf(map.get("result_code")))) {
+            String content = extractStringToSign(res);
+            String sign = sign(content, secret);
+            if (!response.get("sign").equals(sign)) {
+                return true;
+            }
         }
         return false;
     }
@@ -104,10 +120,49 @@ public class Client {
             if (i != 0) {
                 content += "&";
             }
-            content += key + "=" + signedParams.get(key);
+            content += urlEncode(key) + "=" + urlEncode(signedParams.get(key));
 
         }
         return sign(content, secret);
+    }
+
+    /**
+     * URL encode
+     *
+     * @param value the response string
+     * @return the string
+     */
+    public static String urlEncode(final String value) {
+        if (value == null) {
+            return "";
+        }
+
+        try {
+            String encoded = URLEncoder.encode(value, DEFAULT_ENCODING);
+
+            Matcher matcher = ENCODED_CHARACTERS_PATTERN.matcher(encoded);
+            StringBuffer buffer = new StringBuffer(encoded.length());
+
+            while (matcher.find()) {
+                String replacement = matcher.group(0);
+
+                if ("+".equals(replacement)) {
+                    replacement = "%20";
+                } else if ("*".equals(replacement)) {
+                    replacement = "%2A";
+                } else if ("%7E".equals(replacement)) {
+                    replacement = "~";
+                }
+
+                matcher.appendReplacement(buffer, replacement);
+            }
+
+            matcher.appendTail(buffer);
+            return buffer.toString();
+
+        } catch (UnsupportedEncodingException ex) {
+            throw new RuntimeException(ex);
+        }
     }
 
     /**
@@ -118,11 +173,20 @@ public class Client {
      * @return the signature string
      */
     private static String sign(String content, String secret) throws Exception {
-        String charset = "UTF-8";
-        String algorithm = "HmacSHA1";
+        return sign(content, "HmacSHA1", secret, "UTF-8");
+    }
+
+    /**
+     * Calculate signature according to content and secret
+     *
+     * @param content the string to sign
+     * @param secret  the accesskey secret
+     * @return the signature string
+     */
+    private static String sign(String content, String algorithm, String secret, String charset) throws Exception {
         Mac mac = Mac.getInstance(algorithm);
         mac.init(new SecretKeySpec(secret.getBytes(charset), algorithm));
-        mac.update(URLEncoder.encode(content, charset).getBytes(charset));
+        mac.update(content.getBytes(charset));
         byte[] signData = mac.doFinal();
         return Base64.getEncoder().encodeToString(signData);
     }
@@ -142,7 +206,7 @@ public class Client {
             for (Map.Entry<String, String> entry : headers.entrySet()) {
                 conn.setRequestProperty(entry.getKey(), entry.getValue());
             }
-            conn.setRequestMethod("POST");
+            conn.setRequestMethod("PUT");
             conn.setChunkedStreamingMode(0);
             conn.connect();
             OutputStream out = conn.getOutputStream();
@@ -165,10 +229,14 @@ public class Client {
      */
     public static Map<String, String> parseUploadHeaders(Object headers) throws Exception {
         Map<String, String> result = new HashMap<>();
+        if (headers == null) {
+            return result;
+        }
         if (List.class.isAssignableFrom(headers.getClass())) {
             ((List<?>) headers).forEach(item -> {
-                if (Map.class.isAssignableFrom(item.getClass())) {
-                    result.putAll((Map<? extends String, ? extends String>) item);
+                if (TeaModel.class.isAssignableFrom(item.getClass())) {
+                    Map<String, Object> tmp = ((TeaModel) item).toMap();
+                    result.put(tmp.get("name").toString(), tmp.get("value").toString());
                 }
                 if (List.class.isAssignableFrom(item.getClass())) {
                     try {
@@ -180,6 +248,15 @@ public class Client {
             });
         }
         return result;
+    }
+
+    /**
+     * Get nonce
+     *
+     * @return the string
+     */
+    public static String getNonce() {
+        return UUID.randomUUID().toString().replaceAll("-", "");
     }
 
 }
